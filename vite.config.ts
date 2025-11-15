9d386db3a4d6641ee399700d9ae1817bb3535469
funai-yoshihisa-tanaka <yoshihisa-tanaka@funaisoken.co.jp>
2025-11-15 14:18:59 +0900

    設定ファイルの変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
+/// <reference types="vitest" />
+// ↑ 必須
+
 import { reactRouter } from "@react-router/dev/vite";
+import react from '@vitejs/plugin-react';
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config"; // ← 要変更
+import * as path from "path";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), process.env.VITEST ? react() : reactRouter(), tsconfigPaths()], // ← 要変更
+  // ↓ 要追加
+  test: {
+    globals: true,
+    setupFiles: './tests/setup.ts',
+    environment: "jsdom",
+    include: ["tests/**/*.spec.{js,ts,jsx,tsx}"],
+    alias: {
+      "@": path.resolve(__dirname, "./app"),
+    },
+  },
 });
