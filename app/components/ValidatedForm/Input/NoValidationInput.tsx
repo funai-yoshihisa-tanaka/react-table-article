8bd49f20f193eaa9e2fae3d07021188075fbc3ee
funai-yoshihisa-tanaka <yoshihisa-tanaka@funaisoken.co.jp>
2025-11-16 19:49:14 +0900

    コンテキストの構造変更に伴う修正

--- app/components/ValidatedForm/Input/NoValidationInput.tsx
+++ app/components/ValidatedForm/Input/NoValidationInput.tsx
@@ -1,26 +1,37 @@
-import { useEffect, useId } from 'react';
+import { useEffect, useId, useState } from 'react';
 import type { InputHTMLAttributes } from 'react';
 
 import { useFormDispatch } from '../Form';
 
-export function NoValidationInput({onFocus, ...props}: InputHTMLAttributes<HTMLInputElement>) {
-  const set = useFormDispatch();
+export function NoValidationInput({onFocus, onChange, ...props}: InputHTMLAttributes<HTMLInputElement>) {
+  const { setDidPassData, cancelPendingSubmit, setClearFunctions, removeClearFunctions } = useFormDispatch();
+  const [value, setValue] = useState('');
   const myId = useId();
 
   useEffect(() => {
-    set.didPassData((prev) => {
+    setDidPassData((prev) => {
       return {...prev, [myId]: true};
     });
-  }, [myId, set]);
+    setClearFunctions(myId, () => {
+      setValue('');
+    });
+  }, [myId, setDidPassData, setClearFunctions, removeClearFunctions]);
 
   return (
     <input
       onFocus={(e) => {
-        set.event()
+        cancelPendingSubmit()
         if (onFocus) {
           onFocus(e)
         }
       }}
+      onChange={(e) => {
+        setValue(e.target.value);
+        if (onChange) {
+          onChange(e);
+        }
+      }}
+      value={value}
       {...props}
     />
   );
