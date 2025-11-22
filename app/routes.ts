0a3464d7aa48db1e74ee8108379cedf66c324a91
funai-yoshihisa-tanaka <yoshihisa-tanaka@funaisoken.co.jp>
2025-11-22 18:03:04 +0900

    仮エンドポイントの追加

--- app/routes.ts
+++ app/routes.ts
@@ -4,4 +4,5 @@ export default [
   route("/form", "routes/form.tsx"),
   route("/table", "routes/table.tsx"),
   route("/paginated-table", "routes/paginated-table.tsx"),
+  route("/search-paginated-table", "routes/search-paginated-table.tsx"),
 ] satisfies RouteConfig;
